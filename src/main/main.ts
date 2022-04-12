9eab6d65af60a858848a3e458869f4ffa7981b6c
Fergus Lai <ferguslai724@gmail.com>
2022-04-12 11:52:29 +0100

    Set To Full Screen

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,8 +71,9 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1920,
+    height: 1080,
+    fullscreen: true,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
